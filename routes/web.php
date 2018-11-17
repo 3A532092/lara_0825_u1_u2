ad0b1fef1d17a633085fab6e2c223ee8863a9fc1
3A532092 <star1998410@gmail.com>
2018-11-17 20:28:28 +0800

    練習6-2

--- routes/web.php
+++ routes/web.php
@@ -48,8 +48,13 @@ Route::get('/', function () {
 
     //\App\Post::destroy(3,5,7);
     
-    $allPosts = \App\Post::all();
+    /*$allPosts = \App\Post::all();
     dd($allPosts);
     $featuredPosts = \App\Post::where('is_feature', 1)->get();
-    dd($featuredPosts);
+    dd($featuredPosts);*/
+
+    $fourthPost = \App\Post::find(4);
+    dd($fourthPost);
+    $lastPost = \App\Post::orderBy('id', 'DESC')->first();
+    dd($lastPost);
 });
