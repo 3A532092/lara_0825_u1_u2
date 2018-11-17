0ddb6e5f1e8724eca7341c1f524cf9dd6c985a41
3A532092 <star1998410@gmail.com>
2018-11-17 20:57:45 +0800

    練習7-3

--- app/Comment.php
+++ app/Comment.php
@@ -4,8 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Post;
+
 class Comment extends Model
 {
     //
     protected $table='comments';
+
+    public function post(){
+        return $this->belongsTo(Post::class);
+    }
 }
